8d9b629ad3c18d8190a23d63b26847f7ae61c36b
sweeks19 <sweeks@ithaca.edu>
2020-01-26 15:58:04 -0500

    added comments to tester

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -29,18 +29,29 @@ class BankAccountTest {
         //testing prefix
         //numbers, letters, underscore, periods and dashes are ok
         //an underscore, period or dash must be followed by one or more numbers or letters
-        assertFalse( BankAccount.isEmailValid("a-@c.com"));
-        assertFalse(BankAccount.isEmailValid("a..b@c.com"));
-        assertFalse( BankAccount.isEmailValid(".a@c.com"));
-        assertFalse(BankAccount.isEmailValid("a#b@c.com"));
+        assertFalse( BankAccount.isEmailValid("a-@c.com")); //no symbol before @ (border, only 1 symbol)
+        assertFalse(BankAccount.isEmailValid("a..b@c.com")); //double periods (border only 1 following period)
+        assertFalse( BankAccount.isEmailValid(".a@c.com")); //can't start with symbol (border only 1 symbol at start)
+        assertFalse(BankAccount.isEmailValid("a#b@c.com")); //can't use # (border because only 1 #)
+
+        // missing borders:
+        //no symbols before @, multiple symbols before @
+        //single period followed by multiple characters then period, single period one character single period
+        //starting with letter char instead of symbol
+        //multiple #, zero #
 
         //testing incorrect domain
         //letters, numbers and dashes are ok
         //the last portion of the domain must be at least two characters
-        assertFalse( BankAccount.isEmailValid("a@c.c"));
-        assertFalse(BankAccount.isEmailValid("a@c#c.com"));
-        assertFalse( BankAccount.isEmailValid("a@c"));
-        assertFalse(BankAccount.isEmailValid("a@c..com"));
+        assertFalse( BankAccount.isEmailValid("a@c.c")); //domain needs 2 or more character (border only 1 charcter)
+        assertFalse(BankAccount.isEmailValid("a@c#c.com")); //no # in domain (border testing only 1 #)
+        assertFalse( BankAccount.isEmailValid("a@c")); //domain has 0 periods
+        assertFalse(BankAccount.isEmailValid("a@c..com")); //can't have double periods (border only 1 following period)
+
+        //missing borders:
+        //domain having multiple characters
+        //testing 0 #'s, testing multiple #'s
+        // testing more than 2 consecutive periods
 
         //no email at all
         assertFalse( BankAccount.isEmailValid(""));
