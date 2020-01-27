dd233143339c48cf434834a643cbddb2c951ede5
Matthew Becker <mbecker5@ithaca.edu>
2020-01-27 08:35:54 -0500

    Updated withdraw test and get balance test fixed test for email check

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -9,16 +9,70 @@ class BankAccountTest {
     @Test
     void getBalanceTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
         assertEquals(200, bankAccount.getBalance());
+
+        //test for 0
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 0);
+        assertEquals(0, bankAccount2.getBalance());
+
+        //test for negatives this should throw an error
+        //this should also be caught within the constructor
+        BankAccount bankAccount3 = new BankAccount("a@b.com", -200);
+        assertEquals(-200, bankAccount3.getBalance());
+        BankAccount bankAccount4 = new BankAccount("a@b.com", -15);
+        assertEquals(-15, bankAccount4.getBalance());
+        BankAccount bankAccount5 = new BankAccount("a@b.com", -1050);
+        assertEquals(-1050, bankAccount5.getBalance());
+        BankAccount bankAccount6 = new BankAccount("a@b.com", -1050.00);
+        assertEquals(-1050, bankAccount6.getBalance());
+
+        //test for larger positives
+        BankAccount bankAccount7 = new BankAccount("a@b.com", 1000);
+        assertEquals(1000, bankAccount7.getBalance());
+        BankAccount bankAccount8 = new BankAccount("a@b.com", 1000000);
+        assertEquals(1000000, bankAccount8.getBalance());
+        BankAccount bankAccount9 = new BankAccount("a@b.com", 15682);
+        assertEquals(15682, bankAccount9.getBalance());
+        BankAccount bankAccount10 = new BankAccount("a@b.com", 76543.00);
+        assertEquals(76543.00, bankAccount10.getBalance());
     }
 
     @Test
     void withdrawTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
-
         assertEquals(100, bankAccount.getBalance());
+        bankAccount.withdraw(100);
+        assertEquals(0, bankAccount.getBalance());
+
+        //test zero and small amounts
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 200);
+        bankAccount2.withdraw(0);
+        assertEquals(200, bankAccount2.getBalance());
+        bankAccount2.withdraw(5);
+        assertEquals(195, bankAccount2.getBalance());
+        bankAccount2.withdraw(.10);
+        assertEquals(194.90, bankAccount2.getBalance());
+        bankAccount2.withdraw(8.16);
+        assertEquals(186.74, bankAccount2.getBalance());
+
+        //tests greater than amount in account and the exact amount
+        BankAccount bankAccount3 = new BankAccount("a@b.com", 200);
+        bankAccount3.withdraw(1000);
+        assertEquals(200, bankAccount3.getBalance());
+        bankAccount3.withdraw(200.00);
+        assertEquals(0, bankAccount3.getBalance());
+
+        BankAccount bankAccount4 = new BankAccount("a@b.com", 200);
+        bankAccount4.withdraw(-5);
+        assertEquals(200, bankAccount4.getBalance());
+        bankAccount4.withdraw(-150);
+        assertEquals(200, bankAccount4.getBalance());
+        bankAccount4.withdraw(-1000);
+        assertEquals(200, bankAccount4.getBalance());
+        bankAccount4.withdraw(-1500.00);
+        assertEquals(200, bankAccount4.getBalance());
+
     }
 
     @Test
@@ -36,9 +90,20 @@ class BankAccountTest {
 
         // missing borders:
         //no symbols before @, multiple symbols before @
+        // testing more than 2 consecutive periods
+        assertFalse( BankAccount.isEmailValid("a....@c.com"));
+        assertFalse( BankAccount.isEmailValid("@c.com"));
+
         //single period followed by multiple characters then period, single period one character single period
+        assertFalse( BankAccount.isEmailValid(".ab.@c.com"));
+        assertFalse( BankAccount.isEmailValid("a.b.@c.com"));
+
         //starting with letter char instead of symbol
+
         //multiple #, zero #
+        assertFalse( BankAccount.isEmailValid("a#@c.com"));
+        assertFalse( BankAccount.isEmailValid("a##b@c.com"));
+
 
         //testing incorrect domain
         //letters, numbers and dashes are ok
@@ -50,8 +115,17 @@ class BankAccountTest {
 
         //missing borders:
         //domain having multiple characters
+        assertFalse( BankAccount.isEmailValid("a.b@cdf.com"));
+        assertFalse( BankAccount.isEmailValid("a.b@c_f.com"));
+        assertFalse( BankAccount.isEmailValid("a.b@c.d.com"));
+
         //testing 0 #'s, testing multiple #'s
-        // testing more than 2 consecutive periods
+        assertTrue( BankAccount.isEmailValid("a.b123@cdf.com"));
+        assertTrue( BankAccount.isEmailValid("a2.b@c_f.com"));
+        assertTrue( BankAccount.isEmailValid("a.b000@c.d.com"));
+        assertTrue( BankAccount.isEmailValid("a.b@cdf1234.com"));
+        assertTrue( BankAccount.isEmailValid("a.b@c0000000000f.com"));
+        assertTrue( BankAccount.isEmailValid("a.b@c-123434986.com"));
 
         //no email at all
         assertFalse( BankAccount.isEmailValid(""));
